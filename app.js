e5bb3df0bf49848fb85b9eca538c2fd198d2cc18
Gareth Robins <gareth.robins@gmail.com>
2015-03-21 01:21:25 +1300

    connected stream to accelerometer on phone

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*http://java.dzone.com/articles/getting-started-socketio-and*/
+
 var http = require('http'),
     dgram = require('dgram'),
     socketio = require('socket.io');
@@ -5,17 +7,6 @@ var fs = require('fs');
 var app = http.createServer(),
     io = socketio.listen(app),
     socket = dgram.createSocket('udp4');
-/*
-var crlf = new Buffer(2);
-crlf[0] = 0xD; //CR - Carriage return character
-crlf[1] = 0xA; //LF - Line feed character
-*/
-/*
-socket.on('message', function(content, rinfo) {
-   console.log('got message', content.readUInt16LE(0), 'from', rinfo.address, rinfo.port);
-    io.sockets.emit('udp message', content.readUInt16LE(0)); 
-});
-*/
 
     socket.on('message', function (msg) {
         console.log('Message Received: ' + msg);
