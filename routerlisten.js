55b7abb7c7a9f81d7e4e6123ec5e12c6ad7fbbef
Gareth Robins <gareth.robins@gmail.com>
2015-03-20 22:28:32 +1300

    added server file

---
+++ routerlisten.js
@@ -0,0 +1,22 @@
+var dgram = require("dgram");
+
+var server = dgram.createSocket("udp4");
+
+server.on("error", function (err) {
+  console.log("server error:\n" + err.stack);
+  server.close();
+});
+
+server.on("message", function (msg, rinfo) {
+  console.log("server got: " + msg + " from " +
+    rinfo.address + ":" + rinfo.port);
+});
+
+server.on("listening", function () {
+  var address = server.address();
+  console.log("server listening " +
+      address.address + ":" + address.port);
+});
+
+server.bind(33333, '192.168.1.4');
+// server listening 0.0.0.0:33333
